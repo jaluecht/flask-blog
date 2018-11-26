cfc546995d5ab359f0e39c41c26a0ee7042bdc27
Jeff Luecht <jaluecht@gmail.com>
2018-11-26 13:01:47 -0500

    Initial pages

--- blog.py
+++ blog.py
@@ -5,6 +5,9 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'a1b2c3d4e5f6g7h8i9j0' # when in production - generate randum key
 app = Flask(__name__)
 
 # pulls in app configuration by looking for UPPERCASE variables
@@ -14,9 +17,20 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    status_code = 200
+
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+
+    return render_template('login.html', error=error), status_code
 
 @app.route('/main')
 def main():
